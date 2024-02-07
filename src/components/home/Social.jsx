5a6fc5a8c56da4d3a0fe9eee711ea527be4bc821
Ahmed Abaad <ahmedabaad91@gmail.com>
2024-02-07 21:16:38 +0300

    first commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/ahmed__abaad/" className="home__social-icon" target="_black">
+        <i class="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://www.youtube.com/@AhmedAbaad001" className="home__social-icon" target="_black">
+        <i class="uil uil-youtube"></i>
+        </a>
+
+        <a href="https://github.com/ahmed-abaad" className="home__social-icon" target="_black">
+        <i class="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
